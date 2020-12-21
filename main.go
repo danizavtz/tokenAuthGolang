a8fd6c4ecab6223049ff7f1b1876d941b8d97a87
danizavtz <daniellucena@yahoo.com.br>
2020-12-21 08:01:18 -0800

    Fix values for dotenv

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 import (
 	"tokenAuthGolang/server"
 	"fmt"
+	"github.com/subosito/gotenv"
 )
+
+func init() {
+	gotenv.Load()
+}
 func main() {
 	server.LoadRoutes()
 	fmt.Println("hello world")
--- services/database.go
+++ services/database.go
@@ -12,7 +12,7 @@ var dbInstance *pgxpool.Pool
 
 func GetDatabaseInstance()(*pgxpool.Pool) {
 	once.Do(func (){
-		dbi, err := pgxpool.Connect(context.Background(), "postgres://postgres:postgres@0.0.0.0:5432/login?sslmode=disable")
+		dbi, err := pgxpool.Connect(context.Background(), assembleConnectionStringForDatabase())
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 			os.Exit(1)
@@ -20,4 +20,9 @@ func GetDatabaseInstance()(*pgxpool.Pool) {
 		dbInstance = dbi
 	})
 	return dbInstance
+}
+
+func assembleConnectionStringForDatabase() string {
+	connectionStringDatabase := "postgres://"+os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@"+os.Getenv("DB_ADDRESS")+":"+os.Getenv("DB_PORT")+"/"+os.Getenv("DB_NAME")+"?sslmode=disable"
+	return connectionStringDatabase
 }
